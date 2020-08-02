0406e42550e36968f64bcb72ed3114808be99b4b
Rocco Losito <roccolosito@gmail.com>
2020-08-01 22:28:05 -0400

    edited README and connection js

---
+++ db/connection.js
@@ -0,0 +1,17 @@
+var util = require("util");
+var mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  // Your username
+  user: "root",
+  // Your password
+  password: "",
+  database: "employee_DB"
+});
+
+connection.connect();
+
+connection.query = util.promisify(connection.query);
+
+module.exports = connection;
